836c6caef5c324b8eaab4e02380345544c49be53
Satoru Kitaguchi <rule.the.fate.myfirststory@gmail.com>
2021-04-04 15:23:48 +0900

    [add]read request

--- SimpleHTTPServer.java
+++ SimpleHTTPServer.java
@@ -1,10 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.ServerSocket;
+import java.net.Socket;
+
 public class SimpleHTTPServer {
     public static void main(String[] args) throws Exception {
         final ServerSocket server = new ServerSocket(8080);
         System.out.println("listening for connection on port 8080");
         while (true) {
             // spin forever
+            final Socket client = server.accept();
+            // 1. Read HTTP request from the client socket
+            // 2. Prepare an HTTP response
+            // 3. Send HTTP response to the client
+            // 4. Close the socket
+            InputStreamReader isr = new InputStreamReader(client.getInputStream());
+            // Because browser send
+            BufferedReader reader = new BufferedReader(isr);
+            String line = reader.readLine();
+            while (!line.isEmpty()) {
+                System.out.println(line);
+                line = reader.readLine();
+            }
         }
     }
 }
